1705fba78f87c3972abfbc66c7846286b18d977d
Captain Dev <emmanuel@captaindev.io>
2020-05-27 15:34:55 +0200

    Add booking route

--- api/rooms/controllers/rooms.js
+++ api/rooms/controllers/rooms.js
@@ -5,4 +5,20 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  book: async ctx => {
+    const body = ctx.request.body;
+
+    // Validation stuffs
+    if(body.date == null){
+      throw new Error('Validation Error: date should not be null')
+    }
+    if(body.roomId == null){
+      throw new Error('Validation Error: roomId should not be null')
+    }
+
+    const result = await strapi.services.rooms.bookRoom(body.roomId, body.date);
+
+    return result
+  }
+};
--- api/rooms/services/rooms.js
+++ api/rooms/services/rooms.js
@@ -5,4 +5,32 @@
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  bookRoom: async (roomId, date) => {
+
+      const room = await strapi.query('rooms').findOne({id: roomId})
+      const existingBooking = await strapi.query('booking')
+        .findOne({
+          'room.id': roomId,
+          bookingDate: date,
+        })
+
+      // cannot book twice on same day
+      if(existingBooking){
+        throw new Error(`Room ${room.title} is already booked on ${new Date(date).toDateString()}`)
+      }
+
+      const { id, title, images, description, capacity } = room;
+      const newBooking = {
+        bookingDate: date,
+        room: {
+          id, title, images, description, capacity
+        },
+      };
+
+      // Book the room
+      const result = await strapi.query('booking').create(newBooking);
+      return result;
+
+  }
+};
